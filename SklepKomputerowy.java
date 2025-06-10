91bdd5bfa380f51f998b390b8b826e64d010f9f7
s33617 <s33617@pjwstk.edu.pl>
2025-06-10 18:04:14 +0200

    Poprawka

--- SklepKomputerowy.java
+++ SklepKomputerowy.java
@@ -1,51 +1,58 @@
 import java.util.ArrayList;
 
 public class SklepKomputerowy {
-    private ArrayList<Produkt> produkty = new ArrayList<>();
-    private ArrayList<Klient> klienci = new ArrayList<>();
-    private ArrayList<Zamowienie> zamowienia = new ArrayList<>();
-    private int liczbaProduktów = 0;
-    private int liczbaKlientow = 0;
-    private int liczbaZamowien = 0;
+    public Produkt[] produkty = new Produkt[10];
+    public Klient[] klienci =  new Klient[10];
+    public Zamowienie[] zamowienia = new Zamowienie[10];
+    public int liczbaProduktow = 0;
+    public int liczbaKlientow = 0;
+    public int liczbaZamowien = 0;
 
     public void dodajProdukt(Produkt produkt){
-        produkty.add(produkt);
-    }
-    public void dodajKlienta(Klient klient){
-        klienci.add(klient);
-    }
-    public void utworzZamowienie(Klient klienci, Produkt[] produkty, int[] ilosci){
-        Zamowienie zamowienie = new Zamowienie(liczbaZamowien + 1, Klient, produkty, ilosci, "Nowe");
-        zamowienia.add(zamowienie);
-        liczbaZamowien++;
+        if(liczbaProduktow<produkty.length)
+            produkty[liczbaProduktow++] = produkt;
     }
 
-    public void aktualizujStanMagazynowy(Zamowienie zamowienie){
-        for (int i = 0; i < zamowienie.getProdukt().length; i++) {
-            zamowienie.getProdukt()[i].setIloscWMagazynie(zamowienie.getProdukt()[i].getIloscWMagazynie() - zamowienie.getIlosc()[i]);
+    public void dodajKlienta(Klient klient) {
+        if (liczbaKlientow < klienci.length) {
+            klienci[liczbaKlientow++] = klient;
+        }
+    }
+    public Zamowienie utworzZamowienie(Klient klient, Produkt[] produkt, int[] ilosc){
+        Zamowienie noweZamowienie = new Zamowienie(liczbaZamowien+1, klient, produkt, ilosc, "10.06.2025", "Nowe");
+        zamowienia[liczbaZamowien] = noweZamowienie;
+        return zamowienia[liczbaZamowien++];
+    }
+
+    public void aktualizujStanMagazynowy(Zamowienie zamowienie) {
+        Produkt[] produkty = zamowienie.getProdukt();
+        int[] ilosci = zamowienie.getIlosc();
+        for (int i = 0; i < produkty.length; i++) {
+            int aktualnyStan = produkty[i].getIloscWMagazynie();
+            produkty[i].setIloscWMagazynie(aktualnyStan - ilosci[i]);
         }
     }
 
     public void zmienStatusZamowienia(int idZamowienia, String nowyStatus) {
-        for (Zamowienie zamowienie : zamowienia) {
-            if (zamowienie.getId() == idZamowienia) {
-                zamowienie.setStatus(nowyStatus);
+        for(int i = 0; i<liczbaZamowien; i++){
+            if(zamowienia[i].getId() == idZamowienia){
+                zamowienia[i].setStatus(nowyStatus);
             }
         }
     }
 
-    public void wyswietlProduktyWKategorii(String kategoria) {
-        for (Produkt produkt : produkty) {
-            if (produkt.getKategoria().equals(kategoria)) {
-                produkt.wyswietlInformacje();
+    public void wyswietlProduktyWKategorii(String kategoria){
+        for(int i = 0; i<liczbaProduktow; i++){
+            if (produkty[i].getKategoria().equals(kategoria)) {
+                System.out.println(produkty[i].getNazwa() + ", Id:" + produkty[i].getId()+ "," + produkty[i].getCena() + "zł, ilość w magazynie:" + produkty[i].getIloscWMagazynie());
             }
         }
     }
 
     public void wyswietlZamowieniaKlienta(int idKlienta){
-        for (Zamowienie zamowienie : zamowienia) {
-            if (zamowienie.getKlient().getId() == idKlienta) {
-                zamowienie.wyswietlSzczegóły();
+        for(int i = 0; i<liczbaZamowien; i++){
+            if(zamowienia[i].getKlient().getId() == idKlienta){
+                zamowienia[i].wyswietlSzczegóły();
             }
         }
     }
--- Zamowienie.java
+++ Zamowienie.java
@@ -5,29 +5,39 @@ public class Zamowienie {
     private Klient klient;
     private Produkt[] produkt;
     private int[] ilosc; // tablica ilosci odpowiadajacych produktom
-    private String dataZamwienia;
+    private String dataZamowienia;
     private String status; //Status zamowienia
 
-    public double obliczWartoscZamowienia(){
-        double suma = 0;
+    public Zamowienie(int id, Klient klient, Produkt[] produkt, int[] ilosc, String dataZamowienia, String status) {
+        this.id = id;
+        this.klient = klient;
+        this.produkt = produkt;
+        this.ilosc = ilosc;
+        this.dataZamowienia = dataZamowienia;
+        this.status = status;
+    }
+
+    public double obliczWartoscZamowienia() {
+        double suma = 0.0;
         for (int i = 0; i < produkt.length; i++) {
-            suma += produkt[i].getCena()*ilosc[i];
+            suma += produkt[i].getCena() * ilosc[i];
         }
         return suma;
     }
-    public void zastosujZnizke(){
+
+    public void zastosujZnizke() {
         obliczWartoscZamowienia();
-        if (getKlient().isCzyStaly()){
-            double cenaPoZnizce = obliczWartoscZamowienia()*0.9;
+        if (klient.isCzyStaly()) {
+            double cenaPoZnizce = obliczWartoscZamowienia() * 0.9;
         }
     }
 
-    public void wyswietlSzczegóły(){
-        getKlient().wyswietlInformacje();
+    public void wyswietlSzczegóły() {
+        klient.wyswietlInformacje();
         for (int i = 0; i < produkt.length; i++) {
-            getProdukt()[i].wyswietlInformacje();
+            produkt[i].wyswietlInformacje();
         }
-        System.out.println("Data zamówienia: " + dataZamwienia);
+        System.out.println("Data zamówienia: " + dataZamowienia);
         System.out.println("Status zamówienia: " + status);
         System.out.println("Wartość całego Zamówienia: " + obliczWartoscZamowienia());
     }
@@ -40,28 +50,12 @@ public class Zamowienie {
         this.klient = klient;
     }
 
-    public String getStatus() {
-        return status;
+    public int getId() {
+        return id;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getDataZamwienia() {
-        return dataZamwienia;
-    }
-
-    public void setDataZamwienia(String dataZamwienia) {
-        this.dataZamwienia = dataZamwienia;
-    }
-
-    public int[] getIlosc() {
-        return ilosc;
-    }
-
-    public void setIlosc(int[] ilosci) {
-        this.ilosc = ilosci;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Produkt[] getProdukt() {
@@ -72,12 +66,28 @@ public class Zamowienie {
         this.produkt = produkt;
     }
 
-    public int getId() {
-        return id;
+    public int[] getIlosc() {
+        return ilosc;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIlosc(int[] ilosc) {
+        this.ilosc = ilosc;
+    }
+
+    public String getDataZamowienia() {
+        return dataZamowienia;
+    }
+
+    public void setDataZamowienia(String dataZamowienia) {
+        this.dataZamowienia = dataZamowienia;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
 
